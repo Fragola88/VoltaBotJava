e53e71405d7e30d7552f53bc2fa5000507b504c4
daniele-oos <birillomatto72@gmail.com>
2025-02-12 23:01:18 +0100

    Singleton AM

--- src/main/java/controller/ActionManagement.java
+++ src/main/java/controller/ActionManagement.java
@@ -1,6 +1,23 @@
 package controller;
 
+/**
+ * Singleton Obj
+ */
+
 public class ActionManagement {
+
+    static ActionManagement instance;
+
+    private  ActionManagement() {
+
+    }
+
+    static ActionManagement getInstance() {
+        if (instance == null) {
+            instance = new ActionManagement();
+        }
+        return instance;
+    }
 
     /*
     edited by matteo x2
