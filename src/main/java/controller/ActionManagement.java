1dbbe31bae75f1f726b4afdcba71b438ec9aeba0
daniele-oos <birillomatto72@gmail.com>
2025-02-15 13:00:00 +0100

    Update all the MVC pattern

---
+++ src/main/java/controller/ActionManagement.java
@@ -0,0 +1,62 @@
+package controller;
+
+import view.Index;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URI;
+
+public final class ActionManagement extends Index implements Runnable{
+
+
+    public ActionManagement() {
+        super();
+        this.build();
+
+    }
+
+    /**
+     * set the properties for the GUI
+     */
+    private void build() {
+
+        super.collegamentiButton.addActionListener(e -> {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI("https://www.voltafr.edu.it"));
+            } catch (Exception a) {
+                JOptionPane.showMessageDialog(null, "Impossibile aprire la pagina web.", null, JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        send.addActionListener(e -> {
+            if (chatInsert.getText().equals("")) {
+                JOptionPane.showMessageDialog(null, "Inserisci un messaggio.", null, JOptionPane.INFORMATION_MESSAGE);
+            }
+            chatUser.setText(chatInsert.getText());
+        });
+
+    }
+
+
+    /**
+     * start index
+     */
+    public void start() {
+        super.setVisible(true);
+    }
+
+
+    /**
+     * stop index only See,non process in background
+     */
+    public void stop() {
+        super.setVisible(false);
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+}
